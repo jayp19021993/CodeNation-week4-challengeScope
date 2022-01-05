1af788a3337092cd8d64ba1bd6e8c1b7dcf0bc9d
Jay Patel <jay_patel_123@hotmail.co.uk>
2022-01-05 16:39:34 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+
+// //challenge1
+// let greetcode =()=> console.log("hello code nation")
+
+// let printFive = (callback) =>{
+// for (let i =0: i <5; i++){
+// }
+// printFive(greetcode);
+
+
+// //challenge2 
+// function double(n){
+//     return n*2 
+//    let add =()=>{
+//    return 2+3
+//    }
+// } 
+
+// console.log(add +n) 
+
+// //challenge3
+
+
+// //challenge4
+// const multiply = (a,b)=>{
+//     return a*b
+// }
+
+// const add = (a,b)=>{
+//     return a+b
+// }
+
+// const divide = (a,b)=>{
+//     return a/b
+// } 
+ 
+// const subtract = (a,b)=>{
+//     return a-b
+// } 
+
+// const doMath = (num1)=>{
+//     return (num2,fn)=>{
+//         return fn(num1, num2)
+//     }
+// }
